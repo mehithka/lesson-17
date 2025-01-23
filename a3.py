e22dd4fcfc6798f9e4a7aced34234993eb215dd5
mehithka <mehith.karacasia@gmail.com>
2025-01-23 15:41:33 +0500

    a3

---
+++ a3.py
@@ -0,0 +1,12 @@
+import math
+print("I will show you the Ceiling and floor value of 23.26: " + str(math.ceil(23.26))+ ', '+ str(math.floor(23.26)))
+
+x=10
+y=-15
+
+print("the value of x after copying the sign from y is = " +str(math.copysign(x,y)))
+
+print('the absolute value of -96 and 53 is: ' +str(math.fabs(-96)) +', '+str(math.fabs(53)))
+
+print('the GCD of 24 and 56: '+str(math.gcd(24,56)))
+      
